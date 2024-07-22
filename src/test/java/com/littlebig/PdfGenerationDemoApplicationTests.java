4feb211767d881cadecd8ca8a64952b3c56d2715
abaloghb <abaloghbiro@protonmail.com>
2024-07-22 14:15:35 +0200

    Add i18n

--- src/test/java/com/littlebig/PdfGenerationDemoApplicationTests.java
+++ src/test/java/com/littlebig/PdfGenerationDemoApplicationTests.java
@@ -2,12 +2,14 @@ package com.littlebig;
 
 import java.io.FileNotFoundException;
 import java.io.OutputStream;
+import java.util.Locale;
 import java.util.Map;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
 import org.springframework.util.Assert;
 
 @SpringBootTest
@@ -16,10 +18,20 @@ class PdfGenerationDemoApplicationTests {
 	@Autowired
 	private PdfGenerator pdfGenerator;
 
-	@Test
-	void testDemoPdfGeneration() throws FileNotFoundException {
+	@Autowired
+	private MessageSource messageSource;
 
-		OutputStream stream = pdfGenerator.generatePdfContent("demo.html", Map.of("welcomeMessage", "Hello World!"));
+	@Test
+	void testDemoPdfGenerationEN() throws FileNotFoundException {
+
+		OutputStream stream = pdfGenerator.generatePdfContent("demo.html", Map.of("welcomeMessage", messageSource.getMessage("welcomeMessage",null, Locale.ENGLISH)));
+		Assertions.assertNotNull(stream);
+	}
+
+	@Test
+	void testDemoPdfGenerationHU() throws FileNotFoundException {
+
+		OutputStream stream = pdfGenerator.generatePdfContent("demo.html", Map.of("welcomeMessage", messageSource.getMessage("welcomeMessage",null, Locale.of("HU"))));
 		Assertions.assertNotNull(stream);
 	}
 
